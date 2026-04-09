e8be1452bc2285b3499fb051b3289b97a9a312cd
sunnyraghav25 <sunnyraghav801@gmail.com>
2026-04-09 14:41:51 +0530

    Update AddTask.java

--- src/main/java/com/sunny/AddTask.java
+++ src/main/java/com/sunny/AddTask.java
@@ -1,5 +1,5 @@
-package com.sunny;
-package src.main.java;
+package com.sunny; 
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -7,24 +7,16 @@ import javax.servlet.http.*;
 
 @WebServlet("/AddTask")
 public class AddTask extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
 
         String taskName = request.getParameter("t1");
 
-        
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        
         out.println("<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>");
-        out.println("<h1 style='color:green;'>Task Received!</h1>");
-        out.println("<p>Aapka task hai: <b>" + taskName + "</b></p>");
-        out.println("<br><a href='index.html' style='text-decoration:none; color:blue;'>Wapas Jayein</a>");
+        out.println("<h2>Task Added Successfully: " + taskName + "</h2>");
         out.println("</div>");
-        
-    
-        System.out.println("New Task added to list: " + taskName);
     }
 }
